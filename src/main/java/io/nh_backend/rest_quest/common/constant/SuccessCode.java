1cd7cb1a53f78d74ee3715fe7db7d415eaf5becd
황동현 <aaabbb4202@naver.com>
2026-06-04 20:21:28 +0900

    [FEAT] 회원가입 기능 구현
    test로 판단할 것

--- src/main/java/io/nh_backend/rest_quest/common/constant/SuccessCode.java
+++ src/main/java/io/nh_backend/rest_quest/common/constant/SuccessCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessCode {
-    USER_CREATED("로그인 되었습니다.");
+    USER_CREATED("회원가입 되었습니다.");
+
     private final String successMessage;
 }
--- src/test/java/io/nh_backend/rest_quest/RestQuestApplicationTests.java
+++ src/test/java/io/nh_backend/rest_quest/RestQuestApplicationTests.java
@@ -3,7 +3,13 @@ package io.nh_backend.rest_quest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:rest-quest-test;MODE=MySQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class RestQuestApplicationTests {
 
     @Test
