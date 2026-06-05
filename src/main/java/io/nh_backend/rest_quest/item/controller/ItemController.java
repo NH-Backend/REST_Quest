65b10d57a94f9fd4641b3ba9ad6d27a8726b2d0e
nodh020403 <nodh020403n@naver.com>
2026-06-05 09:51:51 +0900

    [FEAT] 아이템 조회 기능 구현 및 entity / response 수정

---
+++ src/main/java/io/nh_backend/rest_quest/item/controller/ItemController.java
@@ -0,0 +1,37 @@
+package io.nh_backend.rest_quest.item.controller;
+
+import io.nh_backend.rest_quest.common.constant.SuccessCode;
+import io.nh_backend.rest_quest.common.dto.ApiResponse;
+import io.nh_backend.rest_quest.item.dto.ItemResponse;
+import io.nh_backend.rest_quest.item.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping
+    public ApiResponse<List<ItemResponse>> getAllItems() {
+        return ApiResponse.ok(
+                itemService.getAllItems(),
+                SuccessCode.ITEM_READ.getSuccessMessage()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<ItemResponse> getItemById(@PathVariable("id") Long id) {
+        return ApiResponse.ok(
+                itemService.getItemById(id), SuccessCode.ITEM_READ_SINGLE.getSuccessMessage()
+        );
+    }
+}
--- src/main/java/io/nh_backend/rest_quest/item/dto/ItemResponse.java
+++ src/main/java/io/nh_backend/rest_quest/item/dto/ItemResponse.java
@@ -1,17 +1,34 @@
 package io.nh_backend.rest_quest.item.dto;
 
+import io.nh_backend.rest_quest.item.domain.Item;
 import io.nh_backend.rest_quest.item.domain.ItemGrade;
 import io.nh_backend.rest_quest.item.domain.ItemType;
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
 public record ItemResponse(
-        Long id,
-        Long rid,
+        Long itemId,
+        String rId,
         String itemName,
-        ItemType itemType,
-        ItemGrade itemGrade,
+        String itemType,
+        String itemGrade,
         String description,
         Integer goldPrice,
         Integer gemPrice,
         Integer sellPrice
 ) {
+    public static ItemResponse from(Item item) {
+        return ItemResponse.builder()
+                .itemId(item.getId())
+                .rId(item.getRId())
+                .itemName(item.getItemName())
+                .itemType(item.getItemType().name())
+                .itemGrade(item.getItemGrade().name())
+                .description(item.getDescription())
+                .goldPrice(item.getGoldPrice())
+                .gemPrice(item.getGemPrice())
+                .sellPrice(item.getSellPrice())
+                .build();
+    }
 }
